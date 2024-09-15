78b99eb22f7f81c9a33d8777dced559b47cb741b
maya-nachiappan <maya.n102304@gmail.com>
2024-09-15 07:30:27 -0700

    testing if sales value fits in categories

--- CIS25 - assignment 10/main.cpp
+++ CIS25 - assignment 10/main.cpp
@@ -10,5 +10,29 @@ using namespace std;
 int main ()
 {
     double sales, commissionRate, commission;
+    cout <<"Enter the value of sales: " << endl;
+    cin >> sales;
     
-}
+    if(sales<10000 && sales>0 )
+    {
+        commissionRate = 10/100;
+    }
+    else if (sales>= 10000 && sales <15000)
+    {
+        commissionRate = 15/100;
+    }
+    else if (sales>=15000)
+    {
+        commissionRate = 20/100;
+    }
+    else
+    {
+        cout << "you have not entered a valid sales value. please re-enter the sales value: " << endl;
+        cin >> sales;
+    }
+    
+    commission = sales*commissionRate;
+    cout << "The commission for the sales value is: "<< commission << endl;
+    return 0;
+    }
+
